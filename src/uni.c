93de7d0c8d3773797e222e89c06432864ce71314
Gabriel Soldani <1268700+gabrielsoldani@users.noreply.github.com>
2023-12-14 13:13:10 -0300

    feat: Add `grapheme_breaks` iterator

--- src/uni.c
+++ src/uni.c
@@ -265,14 +265,77 @@ static int uni_graphemes(lua_State *L)
     return 1;
 }
 
+static int uni_grapheme_breaks_next(lua_State *L)
+{
+    // Retrieve the iterated over string and its length
+    size_t byte_len;
+    const char *str = lua_tolstring(L, lua_upvalueindex(1), &byte_len);
+
+    // First iteration
+    if (lua_isnoneornil(L, lua_upvalueindex(2))) {
+        // If the string is empty, the iterator is immediately exhausted
+        if (byte_len == 0) {
+            lua_pushnil(L);
+            return 1;
+        }
+
+        // Otherwise, the first break is at the beginning of the string
+        lua_pushinteger(L, 0);
+        lua_replace(L, lua_upvalueindex(2));
+
+        lua_pushinteger(L, 1);
+        return 1;
+    }
+
+    // Second and subsequent iterations
+
+    // Retrieve the byte offset for the next iteration
+    lua_Integer off = lua_tointeger(L, lua_upvalueindex(2));
+
+    // Check if the iterator is exhausted
+    if (off >= byte_len) {
+        lua_pushnil(L);
+        return 1;
+    }
+
+    // Find the next grapheme break
+    size_t inc = grapheme_next_character_break_utf8(str + off, byte_len - off);
+
+    off += inc;
+
+    // Check if the iterator is exhausted
+    if (off >= byte_len) {
+        lua_pushnil(L);
+        return 1;
+    }
+
+    // Update the byte offset for the next iteration
+    lua_pushinteger(L, off);
+    lua_replace(L, lua_upvalueindex(2));
+
+    // Push the result, corrected for 1-based indexing
+    lua_pushinteger(L, off + 1);
+
+    return 1;
+}
+
+static int uni_grapheme_breaks(lua_State *L)
+{
+    (void)luaL_checkstring(L, 1);
+    lua_pushnil(L); // initial byte offset
+    lua_pushcclosure(L, uni_grapheme_breaks_next, 2);
+    return 1;
+}
+
 static luaL_Reg funcs[] = {
-    {    "lower",     uni_lower},
-    {    "upper",     uni_upper},
-    {      "len",       uni_len},
-    {  "reverse",   uni_reverse},
-    {      "sub",       uni_sub},
-    {"graphemes", uni_graphemes},
-    {       NULL,          NULL}
+    {          "lower",           uni_lower},
+    {          "upper",           uni_upper},
+    {            "len",             uni_len},
+    {        "reverse",         uni_reverse},
+    {            "sub",             uni_sub},
+    {"grapheme_breaks", uni_grapheme_breaks},
+    {      "graphemes",       uni_graphemes},
+    {             NULL,                NULL}
 };
 
 int luaopen_uni(lua_State *L)

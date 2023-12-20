854040335fe90f7d4dd619a23f8142a4e940a0eb
Gabriel Soldani <1268700+gabrielsoldani@users.noreply.github.com>
2023-12-20 17:20:54 -0300

    feat: Add `lpeg_ext.GraphemeCount`

--- src/lpeg_ext.lua
+++ src/lpeg_ext.lua
@@ -4,5 +4,8 @@ local uni = require("uni")
 local lpeg_ext = {}
 
 lpeg_ext.GraphemeBreak = lpeg.P(uni.is_grapheme_break)
+lpeg_ext.GraphemeCount = function(count)
+   return lpeg.P(uni._lpeg_ext_GraphemeCount(count))
+end
 
 return lpeg_ext
